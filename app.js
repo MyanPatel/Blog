e52c8e0a0035a83a3211199e0e21f8205d3b5a52
Myan <myanpatel98@gmail.com>
2020-06-26 13:09:29 +0100

    Challenge 18: Lodash package used to improve blog post searching

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 // Some sample content to put on the Home, About and Contact pages
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -75,9 +76,9 @@ app.get("/posts/:postTitle", function(req, res){
 
   // console.log(req.params.postTitle);
 
-  let requestedTitle = req.params.postTitle;
+  let requestedTitle = _.lowerCase(req.params.postTitle);
   posts.forEach(function(post){
-    let storedTitle = post.title;
+    let storedTitle = _.lowerCase(post.title);
     if (storedTitle === requestedTitle){
       console.log("Match Found!");
     }
